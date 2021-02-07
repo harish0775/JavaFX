395cca39967cf5781127a0c7ba29530a9dd253d0
Harish Nishad <harish.nishad0775@gmail.com>
2021-02-07 19:50:10 +0530

    JFX_Property

---
+++ DemoFX/src/JFXproperty.java
@@ -0,0 +1,38 @@
+
+
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.*;
+
+
+public class JFXproperty extends Application
+{
+    public void start(Stage stage) throws Exception
+    {
+        Button b=new Button("Click _Me"); // clicked button box
+        b.setTextFill(Color.BLACK); //clicked box color
+        b.setMnemonicParsing(true); //under line
+        Tooltip tp=new Tooltip("Click this Button to Save Data");  // hover over tool tip show
+        b.setTooltip(tp);
+       b.setStyle("-fx-border-color:yellow;-fx-background-color:red");//set border&background color
+        Alert a=new Alert(AlertType.INFORMATION,"Button is Clicked"); // alert pop up , alert typer Information 
+          // different type of alert : information error,warning,conformation etc
+       b.setOnAction(e->a.show());
+       
+        FlowPane fp=new FlowPane(b);
+        
+        Scene sc=new Scene(fp,400,400);
+        stage.setScene(sc);
+        stage.show();
+        
+        
+    }
+    public static void main(String[] args) 
+    {
+        launch(args);
+    }   
+}
